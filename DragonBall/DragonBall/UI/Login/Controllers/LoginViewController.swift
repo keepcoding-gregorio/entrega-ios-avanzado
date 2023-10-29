2cf03390fe4a3b2eb71106adbad681a77564718c
Gonzalo Gregorio <gonzagregorio03@gmail.com>
2023-10-29 10:59:39 -0300

    Define project architecture

---
+++ DragonBall/DragonBall/UI/Login/Controllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  DragonBall
+//
+//  Created by Gonzalo Gregorio on 29/10/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
